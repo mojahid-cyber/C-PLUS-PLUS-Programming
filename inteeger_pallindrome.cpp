4778508bc444fa1396f6b01b3af0a6944290b501
mojahid-cyber <mojahida810@gmail.com>
2026-08-24 09:01:32 +0530

    Add files via upload

---
+++ inteeger_pallindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, original, reverse = 0, remainder;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    original = n;
+
+    while (n != 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reverse) {
+        cout << "The number is a palindrome.";
+    } else {
+        cout << "The number is not a palindrome.";
+    }
+
+    return 0;
+}
